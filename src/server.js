3ec36a575350c57b9cb6c8e4b4b91c77abaf8fd4
Jeanine Hoffman <jeanine.hoffman1970@gmail.com>
2019-10-01 19:50:40 -0700

    heroku changes in server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,19 @@ const { PORT, DATABASE_URL } = require('./config')
 const db = knex({ 
   client: 'pg', 
   connection: DATABASE_URL, 
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 app.set('db', db);
 
